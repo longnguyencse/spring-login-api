81234f5c08f3e2350efeb80cb616d20d23911d19
longnq <longnq@itvsoft.vn>
2019-05-06 15:19:37 +0700

    ++ comment : url swagger

--- src/main/java/kalog/com/vn/polls/config/SwaggerConfig.java
+++ src/main/java/kalog/com/vn/polls/config/SwaggerConfig.java
@@ -11,6 +11,7 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
 /**
  * Author Mikorn vietnam
  * Created on 14-Feb-19.
+ * Link : http://localhost:5000/swagger-ui.html
  */
 
 @Configuration
